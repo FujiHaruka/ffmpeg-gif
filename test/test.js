5491ac2e3af75a5dcbb92cd150a80ed01449ddf3
FujiHaruka <fizzyjohn1427@gmail.com>
2019-10-06 23:06:34 +0900

    Improve test

--- test/test.js
+++ test/test.js
@@ -3,4 +3,9 @@ const gify = require('../gify')
 
 process.chdir(path.join(__dirname, '..'))
 
-gify('test/test.mp4', 'tmp/out.gif')
+gify('test/test.mp4', 'tmp/out.gif', {
+  fps: '5',
+  scale: '120:-1',
+  seek: '2',
+  time: '3',
+})
